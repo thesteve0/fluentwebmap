304c5dc7b07c8c5499a8a62c7c833c84bc89fb3d
Steven Pousty <spousty@redhat.com>
2014-02-11 13:38:46 -0800

    trying first push

--- README.md
+++ README.md
@@ -1,3 +1,52 @@
-The OpenShift `nodejs` cartridge documentation can be found at:
+#The code to accompany Fluent Webinar 2014
 
-https://github.com/openshift/origin-server/tree/master/cartridges/openshift-origin-cartridge-nodejs/README.md
+This code actually belongs to a [blog piece](https://openshift.redhat.com/community/blogs/using-nodejs-mongodb-express-for-your-spatial-web-service-and-its-free) written on OpenShift.com 
+
+========
+
+Running on OpenShift
+----------------------------
+
+Create an account at http://openshift.redhat.com/
+
+Create a Node.js application and add a MongoDB cartridge to the app
+
+    rhc app create parkmapsnode nodejs-0.10 mongodb-2.2
+
+Add this upstream node repo
+
+
+    cd parkmapsnode
+    git remote add upstream -m master https://??????
+    git pull -s recursive -X theirs upstream master
+    
+Then push the repo upstream
+
+    git push
+    
+
+Now, ssh into the application.
+
+Add the data to a collection called parkpoints:
+
+//This should actually go in the deploy hook
+    mongoimport -d parkmapsnode -c parkpoints --type json --file $OPENSHIFT_REPO_DIR/parkcoord.json  -h $OPENSHIFT_MONGODB_DB_HOST  -u admin -p $OPENSHIFT_MONGODB_DB_PASSWORD
+
+    
+Create the spatial index on the documents:
+
+    mongo
+    use nodews
+    db.parkpoints.ensureIndex( { pos : "2d" } );
+
+Once the data is imported you can now checkout your application at:
+
+    http://nodews-$yournamespace.rhcloud.com/ws/parks
+
+
+License
+-------
+
+This code is dedicated to the public domain to the maximum extent
+permitted by applicable law, pursuant to CC0
+http://creativecommons.org/publicdomain/zero/1.0/
--- server.js
+++ server.js
@@ -1,159 +1,153 @@
 #!/bin/env node
-//  OpenShift sample Node application
+
 var express = require('express');
 var fs      = require('fs');
+var mongodb = require('mongodb');
+
+var App = function(){
+
+  // Scope
+  var self = this;
+
+  // Setup
+  self.dbServer = new mongodb.Server(process.env.OPENSHIFT_MONGODB_DB_HOST,parseInt(process.env.OPENSHIFT_MONGODB_DB_PORT));
+  self.db = new mongodb.Db('nodews', self.dbServer, {auto_reconnect: true});
+  self.dbUser = process.env.OPENSHIFT_MONGODB_DB_USERNAME;
+  self.dbPass = process.env.OPENSHIFT_MONGODB_DB_PASSWORD;
+
+  self.ipaddr  = process.env.OPENSHIFT_NODEJS_IP;
+  self.port    = parseInt(process.env.OPENSHIFT_NODEJS_PORT) || 8080;
+  if (typeof self.ipaddr === "undefined") {
+    console.warn('No OPENSHIFT_NODEJS_IP environment variable');
+  };
 
 
-/**
- *  Define the sample application.
- */
-var SampleApp = function() {
+  // Web app logic
+  self.routes = {};
+  self.routes['health'] = function(req, res){ res.send('1'); };
+  
+  self.routes['root'] = function(req, res){res.send('You have come to the park apps web service. All the web services are at /ws/parks*. For example /ws/parks will return all the parks in the system in a JSON payload. Thanks for stopping by and have a nice day'); };
 
-    //  Scope.
-    var self = this;
+  //returns all the parks in the collection
+  self.routes['returnAllParks'] = function(req, res){
+    self.db.collection('parkpoints').find().toArray(function(err, names) {
+        res.header("Content-Type:","application/json");
+        res.end(JSON.stringify(names));
+    });
+  };
+
+  //find a single park by passing in the objectID to the URL
+  self.routes['returnAPark'] = function(req, res){
+      var BSON = mongodb.BSONPure;
+      var parkObjectID = new BSON.ObjectID(req.params.id);
+      self.db.collection('parkpoints').find({'_id':parkObjectID}).toArray(function(err, names){
+              res.header("Content-Type:","application/json");
+              res.end(JSON.stringify(names));
+      });
+  };
 
 
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
+  //find parks near a certain lat and lon passed in as query parameters (near?lat=45.5&lon=-82)
+  self.routes['returnParkNear'] = function(req, res){
+      //in production you would do some sanity checks on these values before parsing and handle the error if they don't parse
+      var lat = parseFloat(req.query.lat);
+      var lon = parseFloat(req.query.lon);
 
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+      self.db.collection('parkpoints').find( {"pos" : {$near: [lon,lat]}}).toArray(function(err,names){
+          res.header("Content-Type:","application/json");
+          res.end(JSON.stringify(names));
+       });
+  };
 
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
+
+  self.routes['returnParkNameNear'] = function(req, res){
+      var lat = parseFloat(req.query.lat);
+      var lon = parseFloat(req.query.lon);
+      var name = req.params.name;
+      self.db.collection('parkpoints').find( {"Name" : {$regex : name, $options : 'i'}, "pos" : { $near : [lon,lat]}}).toArray(function(err,names){
+          res.header("Content-Type:","application/json");
+          res.end(JSON.stringify(names));
+      });
+  };
+
+  self.routes['postAPark'] = function(req, res){
+
+     var name = req.body.name;
+     var lat = req.body.lat;
+     var lon = req.body.lon;
+     console.log(req.body);
+
+     self.db.collection('parkpoints').insert({'Name' : name, 'pos' : [lon,lat ]}), function(result){
+         //we should have caught errors here for a real app
+         res.end('success');
+     };
+  };
+
+
+
+
+
+  // Web app urls
+  
+  self.app  = express();
+
+  //This uses the Connect frameworks body parser to parse the body of the post request
+  self.app.configure(function () {
+        self.app.use(express.bodyParser());
+        self.app.use(express.methodOverride());
+        self.app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  });
+
+  //define all the url mappings
+  self.app.get('/health', self.routes['health']);
+  self.app.get('/', self.routes['root']);
+  self.app.get('/ws/parks', self.routes['returnAllParks']);
+  self.app.get('/ws/parks/park/:id', self.routes['returnAPark']);
+  self.app.get('/ws/parks/near', self.routes['returnParkNear']);
+  self.app.get('/ws/parks/name/near/:name', self.routes['returnParkNameNear']);
+  self.app.post('/ws/parks/park', self.routes['postAPark']);
+
+  // Logic to open a database connection. We are going to call this outside of app so it is available to all our functions inside.
+
+  self.connectDb = function(callback){
+    self.db.open(function(err, db){
+      if(err){ throw err };
+      self.db.authenticate(self.dbUser, self.dbPass, {authdb: "admin"}, function(err, res){
+        if(err){ throw err };
+        callback();
+      });
+    });
+  };
+  
+  
+  //starting the nodejs server with express
+  self.startServer = function(){
+    self.app.listen(self.port, self.ipaddr, function(){
+      console.log('%s: Node server started on %s:%d ...', Date(Date.now()), self.ipaddr, self.port);
+    });
+  }
+
+  // Destructors
+  self.terminator = function(sig) {
+    if (typeof sig === "string") {
+      console.log('%s: Received %s - terminating Node server ...', Date(Date.now()), sig);
+      process.exit(1);
     };
+    console.log('%s: Node server stopped.', Date(Date.now()) );
+  };
 
+  process.on('exit', function() { self.terminator(); });
 
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
-        }
+  self.terminatorSetup = function(element, index, array) {
+    process.on(element, function() { self.terminator(element); });
+  };
 
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
+  ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT', 'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGPIPE', 'SIGTERM'].forEach(self.terminatorSetup);
 
+};
 
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function(key) { return self.zcache[key]; };
+//make a new express app
+var app = new App();
 
-
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
-
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
-
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
-        });
-    };
-
-
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
-
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
-
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
-        };
-    };
-
-
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express.createServer();
-
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
-    };
-
-
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
-
-        // Create the express server and routes.
-        self.initializeServer();
-    };
-
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
-        //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
-        });
-    };
-
-};   /*  Sample Application.  */
-
-
-
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
-
+//call the connectDb function and pass in the start server command
+app.connectDb(app.startServer);
